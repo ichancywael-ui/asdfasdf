48ddf68054dec59ebc3129f39b5de2539d3df7a9
asjdfh435 <ichancywael@gmail.com>
2026-06-09 19:20:22 +0300

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -75,6 +75,7 @@ router.include_router(withdraw_router)
 
 @router.message(lambda m: m.text and m.text.startswith("/start"))
 async def start(m: types.Message):
+    await state.clear()
     user_id = m.from_user.id
     args = m.text.split(maxsplit=1)
 
